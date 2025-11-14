b8e67229e3d146a6ef4c7624b56bb96a254dc514
Sydney Valliere <126.sydney.v@gmail.com>
2025-11-14 10:35:11 -0500

    Add project files.

---
+++ Lottery Picker/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Lottery_Picker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(661, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lottery Number Generator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(200, 158);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(225, 57);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputLabel.Font = new System.Drawing.Font("Lucida Bright", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(129, 296);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(382, 70);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lottery Picker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Lottery Picker/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Picker
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        Random randGen = new Random();
+        int max = 50;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            //create random values for each number
+           int int1 = randGen.Next(1,max);
+           int int2 = randGen.Next(1,max);
+           int int3 = randGen.Next(1,max);
+           int int4 = randGen.Next(1,max);
+           int int5 = randGen.Next(1,max);
+           int int6 = randGen.Next(1,max);
+            //if one number = another it picks a new number
+            while(int2 == int1)
+            { int2 = randGen.Next(1,max);}
+            while(int3 == int2 | int3 == int1) 
+            {int3 = randGen.Next(1,max);}
+            while (int4 == int3 | int4 == int2 | int4 == int1)
+            { int4 = randGen.Next(1,max);}
+            while (int5 == int4 | int5 == int3 | int5 == int2 | int5 == int1)
+            { int5 = randGen.Next(1,max);}
+            while (int6 == int5 | int6 == int4 | int6 == int3 | int6 == int2 | int6 == int1)
+            { int6 = randGen.Next(1,max);}
+
+            //display numbers
+            outputLabel.Text = int1.ToString() + " " + int2.ToString() + " " + int3.ToString() + " " + int4.ToString() + " " + int5.ToString() + " " + int6.ToString();
+        }
+    }
+}
